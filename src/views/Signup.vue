7cb80b553945e7243721a8032c138785d06abf51
vproskurin <vladimir.proskurin@gmail.com>
2020-07-21 08:41:01 -0700

    form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
+        <v-form ref="signUpForm" v-model="formValidity">
           <v-text-field
             label="Email"
             type="email"
@@ -28,7 +28,20 @@
             v-bind:rules="agreeToTermsRules"
             required
           ></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-btn
+            class="mr-4"
+            type="submit"
+            color="primary"
+            :disabled="!formValidity"
+            >Submit</v-btn
+          >
+          <v-btn class="mr-4" color="success" @click="validateForm"
+            >Validate Form</v-btn
+          >
+          <v-btn class="mr-4" color="warning" @click="resetValidation"
+            >Reset Validation</v-btn
+          >
+          <v-btn class="mr-4" color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -38,25 +51,37 @@
 <script>
 export default {
   data: () => ({
+    formValidity: false,
     agreeToTerms: false,
-    agreeToTermsRules: [(value) => !!value || "You must agree to terms"],
-    email: "",
+    agreeToTermsRules: [value => !!value || 'You must agree to terms'],
+    email: '',
     emailRules: [
-      (value) => !!value || "Email is required",
-      (value) => value.indexOf("@") !== 0 || "Email should have a username",
-      (value) => value.includes("@") || "Email should be valid",
-      (value) =>
-        value.indexOf(".") - value.indexOf("@") > 1 ||
-        "Email should contain a valid domain.",
-      (value) => value.includes(".") || "Email should include a period symbol.",
-      (value) =>
-        value.indexOf(".") <= value.length - 3 ||
-        "Email should contain a valid domain extension.",
+      value => !!value || 'Email is required',
+      value => value.indexOf('@') !== 0 || 'Email should have a username',
+      value => value.includes('@') || 'Email should be valid',
+      value =>
+        value.indexOf('.') - value.indexOf('@') > 1 ||
+        'Email should contain a valid domain.',
+      value => value.includes('.') || 'Email should include a period symbol.',
+      value =>
+        value.indexOf('.') <= value.length - 3 ||
+        'Email should contain a valid domain extension.'
     ],
-    birthday: "",
-    browsers: ["Chrome", "Firefox", "Safari", "Edge"],
+    birthday: '',
+    browsers: ['Chrome', 'Firefox', 'Safari', 'Edge']
   }),
-};
+  methods: {
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation()
+    },
+    resetForm() {
+      this.$refs.signUpForm.reset()
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate()
+    }
+  }
+}
 </script>
 
 <style></style>
