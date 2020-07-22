d6862aad1b0637b71fce63947b5a30ee4fc7f2cb
vproskurin <vladimir.proskurin@gmail.com>
2020-07-22 08:38:49 -0700

    directives and intersection observer

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,7 +7,7 @@
       </v-col>
     </v-row>
 
-    <v-row>
+    <v-row> 
       <v-col v-for="statistic in statistics" :key="`${statistic.title}`" >
         <StatisticCard :statistic="statistic" />
       </v-col>
@@ -20,6 +20,26 @@
         <EventTimeline :timeline="timeline" />
       </v-col>
     </v-row>
+
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row  v-if="loadNewContent"  id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
@@ -49,6 +69,7 @@ export default {
   },
   data() {
     return {
+      loadNewContent: false,
       employees: employeesData,
       sales: salesData,
       selectedEmployee: {
@@ -66,6 +87,11 @@ export default {
       this.selectedEmployee.name = event.name;
       this.selectedEmployee.title = event.title;
     },
+    showMoreContent(entries){
+      this.loadNewContent = entries[0].isIntersecting
+
+
+    }
   },
 };
 </script>
